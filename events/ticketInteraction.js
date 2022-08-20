fe9560e5822adc0169b8c4a0c048eedb1f4497d7
LunarCodes <91988772+LunarTaku@users.noreply.github.com>
2022-08-21 00:14:16 +0300

    Major changes

--- events/ticketInteraction.js
+++ events/ticketInteraction.js
@@ -1,7 +1,5 @@
 const {
   Client,
-  CommandInteraction,
-  InteractionType,
   ChatInputCommandInteraction,
   EmbedBuilder,
   ButtonBuilder,
@@ -10,6 +8,7 @@ const {
   ChannelType,
   PermissionFlagsBits,
 } = require("discord.js");
+
 const ticketSchema = require("../../schemas/ticketSchema");
 const { createTranscript } = require("discord-html-transcripts");
 
@@ -24,6 +23,8 @@ module.exports = {
   async execute(interaction, client) {
     if (!interaction.isButton()) return;
 
+    const ID = Math.floor(Math.random() * 90000);
+
     const config = await ticketSchema.findOne({
       guildID: interaction.guild.id,
     });
@@ -37,15 +38,15 @@ module.exports = {
 
       // Check if config does not exist, if it does it will create one.
       if (!config) {
+        const Reply = new EmbedBuilder()
+          .setTitle("Ticket System")
+          .setDescription(
+            `You are required to set up the ticket system before using it! Please use \`/ticket setup\` to set it !up`
+          )
+          .setColor(colors.red);
+
         await interaction.reply({
-          embeds: [
-            new EmbedBuilder()
-              .setTitle("Ticket System")
-              .setDescription(
-                "You have not set up the ticket system yet! Use `/ticket setup` to set it up!"
-              )
-              .setColor("Red"),
-          ],
+          embeds: [Reply],
           ephemeral: true,
         });
         return;
@@ -64,8 +65,9 @@ module.exports = {
 
       // Create the ticket channel
       const channel = await category.children.create({
-        name: `ticket-${Math.floor(Math.random() * 100000)}`,
+        name: `ticket-${interaction.user.username}${interaction.user.discriminator}`,
         type: ChannelType.GuildText,
+        topic: interaction.user.id,
         permissionOverwrites: [
           {
             id: interaction.guild.roles.everyone.id,
@@ -90,111 +92,159 @@ module.exports = {
       });
 
       // send the "ticket created" message
-      if (config.embedDescription) {
-        const buttonComp = new ActionRowBuilder().setComponents(
-          new ButtonBuilder()
-            .setCustomId("ticket-close")
-            .setLabel("Close Ticket")
-            .setStyle(ButtonStyle.Danger),
-          new ButtonBuilder()
-            .setCustomId("saveChat")
-            .setLabel("Save Transcript")
-            .setStyle(ButtonStyle.Secondary)
-        );
+      const TicketActions = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("ticket-close")
+          .setLabel("Close Ticket")
+          .setStyle(ButtonStyle.Danger)
+          .setDisabled(false),
+        new ButtonBuilder()
+          .setCustomId("ticket-claim")
+          .setLabel("Claim Ticket")
+          .setStyle(ButtonStyle.Success)
+          .setDisabled(false)
+      );
 
-        const ticketMsg = new EmbedBuilder()
-          .setAuthor({
-            name: interaction.user.username,
-            iconURL: interaction.user.avatarURL(),
-          })
-          .setTitle(`${interaction.user.username} has created a ticket!`)
-          .setDescription(config.embedDescription)
-          .setColor("Random");
+      const StartEmbed = new EmbedBuilder()
+        .setAuthor({
+          name: interaction.user.username,
+          iconURL: interaction.user.avatarURL(),
+        })
+        .setTitle(`Ticket-${interaction.user.username}#${interaction.user.discriminator}`)
+        .setDescription(`Welcome **${interaction.user.username}** to this ticket!\nPlease wait for a staff member to reply to your ticket, or if you created it by accidentally please use the "close ticket" button to close it.`)
+        .setColor("Blurple");
 
-        channel.send({
-          embeds: [ticketMsg],
-          components: [buttonComp],
-        });
+      channel.send({
+        embeds: [StartEmbed],
+        components: [TicketActions],
+      });
 
-      } else if (config.embedDescription == null) {
-        channel.send({
-          embeds: [
-            new EmbedBuilder()
-              .setAuthor({
-                name: interaction.user.username,
-                iconURL: interaction.user.avatarURL(),
-              })
-              .setTitle(`${interaction.user.username} has created a ticket!`)
-              .setDescription(
-                `welcome ${interaction.user.username} to this ticket! Please wait for a staff member to reply to your ticket, or if you created it by accidentally please use the "close ticket" button to close it.`
-              )
-              .setColor("Random"),
-          ],
-          components: [
-            new ActionRowBuilder().setComponents(
-              new ButtonBuilder()
-                .setCustomId("ticket-close")
-                .setLabel("Close Ticket")
-                .setStyle(ButtonStyle.Danger),
-              new ButtonBuilder()
-                .setCustomId("saveChat")
-                .setLabel("Save Transcript")
-                .setStyle(ButtonStyle.Secondary)
-            ),
-          ],
-        });
-      }
+      const Reply = new EmbedBuilder()
+        .setDescription(`Your ticket has been successfully created!`)
+        .setColor("Green");
 
       await interaction.followUp({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription("Your ticket has been successfully created!")
-            .setColor("Green"),
-        ],
+        embeds: [Reply],
         ephemeral: true,
       });
-    }
+    } 
     // checks if user clicked the "close ticket" button
     else if (interaction.customId == "ticket-close") {
-      await interaction.reply({
-        embeds: [
-          new EmbedBuilder().setDescription(
-            "<:FIJI_clock:999673593170509945> Ticket will be deleted in T-10 seconds!"
-          ),
-        ],
-        ephemeral: true,
-      });
-      setTimeout(() => {
-        interaction.channel.delete();
-      }, 10000).catch((err) => {
-        console.log(err);
-      });
-    }
-    // checks if user clicked the "save transcript" button and saves the transcript
-    else if (interaction.customId == "saveChat") {
-      await interaction.deferReply();
+      if (!interaction.member.roles.cache.has(config.supportRole))
+        return interaction.reply({
+          content: `<@${interaction.user.id}> You require the support role to claim a ticket.`,
+          ephemeral: true,
+        });
+
       const { channel } = interaction;
+
+      // Buttons
+      const DisabledClose = new ActionRowBuilder().setComponents(
+        new ButtonBuilder()
+          .setCustomId("ticket-close")
+          .setLabel("Close Ticket")
+          .setStyle(ButtonStyle.Danger)
+          .setDisabled(true)
+      );
+
+      // Some Embeds
       const reply = new EmbedBuilder()
-        .setTitle(`Transcript saved`)
-        .setDescription(`Saved Transcript: <#${config.transcriptChannel}>`)
-        .setColor("BLUE");
+        .setDescription(
+          `The ticket has been closed by **${interaction.user.tag}**`
+        )
+        .setColor("Red");
+      const Embed = new EmbedBuilder()
+        .setColor("Red")
+        .setDescription(
+          `The ticket is closing please wait 10 seconds until it gets deleted.`
+        );
+
+      const EmbedDM = new EmbedBuilder()
+        .setTitle(`Ticket Closed!`)
+        .setColor("Red")
+        .setFields({
+          name: `Information:`,
+          value: `
+          **Guild Name:** ${interaction.guild.name}
+          **Guild Id:** ${interaction.guild.id}
+          **Created By:** <@!${channel.topic}>
+          **Ticket ID:** ${ID}
+          **Closed By:** ${interaction.user.tag}
+          `,
+        })
+        .setFooter({ text: "The ticket was closed at" })
+        .setTimestamp();
 
       const attachment = await createTranscript(channel, {
         limit: -1,
         returnBuffer: false,
-        fileName: `${channel}.html`,
+        fileName: `Ticket-${ID}.html`,
       });
 
-      client.channels.cache.get(config.transcriptChannel).send({
+      interaction.message.edit({
+        embeds: [Embed],
+        components: [DisabledClose],
+      });
+
+      await interaction.reply({
         embeds: [reply],
-        files: [attachment],
+        ephemeral: false,
       });
 
-      await interaction.followUp({
-        content: `Transcript has been successfully saved to <#${config.transcriptChannel}>!`,
-        ephemeral: true,
+      setTimeout(() => {
+        client.channels.cache.get(config.ticketlog).send({
+          embeds: [EmbedDM],
+          files: [attachment],
+        });
+      }, 10000);
+
+      setTimeout(() => {
+        interaction.channel.delete();
+      }, 10000);
+    }
+    // Checks if a user clicked ticket claim
+    else if (interaction.customId == "ticket-claim") {
+      if (!interaction.member.roles.cache.has(config.supportRole))
+        return interaction.reply({
+          content: `<@${interaction.user.id}> You require the support role to claim a ticket.`,
+          ephemeral: true,
+        });
+      const DisabledClaim = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("ticket-close")
+          .setLabel("Close Ticket")
+          .setStyle(ButtonStyle.Danger)
+          .setDisabled(false),
+        new ButtonBuilder()
+          .setCustomId("ticket-claim")
+          .setLabel("Claim Ticket")
+          .setStyle(ButtonStyle.Success)
+          .setDisabled(true)
+      );
+
+      const Embed = new EmbedBuilder()
+        .setAuthor({
+          name: interaction.user.username,
+          iconURL: interaction.user.avatarURL(),
+        })
+        .setTitle(`Ticket-${interaction.user.username}#${interaction.user.discriminator}`)
+        .setDescription(`Welcome **${interaction.user.username}** to this ticket!\nPlease wait for a staff member to reply to your ticket, or if you created it by accidentally please use the "close ticket" button to close it.`)
+        .setColor("Blurple");
+
+      interaction.message.edit({
+        embeds: [Embed],
+        components: [DisabledClaim],
+      });
+      const reply = new EmbedBuilder()
+        .setDescription(
+          `Ticket has been claimed by **${interaction.user.tag}**!`
+        )
+        .setColor("Green");
+
+      await interaction.reply({
+        embeds: [reply],
+        ephemeral: false,
       });
     }
   },
 };
-
